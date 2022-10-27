49d1e7282f1731c600d551d65930e623505b6f50
Gabriel Medrado <gabriel.medradoo@hotmail.com>
2022-10-27 16:20:22 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { Header } from "./components/Header/header";
+import { Post } from "./components/Post/post";
+import { Sidebar } from "./components/Sidebar/sidebar";
+
+import styles from "./App.module.css";
+import "./global.css";
+
+export function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+          <Post
+            author="Gabriel"
+            content="Lorem ipsum dolor sit amet. Eum natus consequatur sit voluptatem dolores est totam deserunt. Qui sint magnam ex doloremque odio non natus eligendi sit officia fugit et voluptas quisquam."
+          />
+          <Post
+            author="Xeke"
+            content="Lorem ipsum dolor sit amet. Eum natus consequatur sit voluptatem dolores est totam deserunt. Qui sint magnam ex doloremque odio non natus eligendi sit officia fugit et voluptas quisquam."
+          />
+        </main>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Sidebar/sidebar.jsx
@@ -0,0 +1,31 @@
+import { PencilLine } from "phosphor-react";
+
+import styles from "./sidebar.module.css";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1666542061821-172d68a9d875?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDE3fGJvOGpRS1RhRTBZfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+      />
+
+      <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://github.com/GabrielMedradoS.png"
+        />
+
+        <strong>Gabriel Medrado</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
