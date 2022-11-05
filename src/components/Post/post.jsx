8c87bfc4fd7e87d121c5a335c68af48db90da87a
Gabriel Medrado <gabriel.medradoo@hotmail.com>
2022-11-05 19:52:24 -0300

    post

--- src/components/Post/post.jsx
+++ src/components/Post/post.jsx
@@ -1,8 +1,37 @@
+import styles from "./post.module.css";
+
 export function Post(props) {
   return (
-    <div>
-      <h3>{props.author}</h3>
-      <p>{props.content}</p>
-    </div>
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/GabrielMedradoS.png"
+          />
+          <div className={styles.authorInfo}>
+            <strong>Gabriel Medrado</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time title="" dateTime="">
+          Publicado há 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala Galera</p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um porjeto que fiz
+          no NLW Return, evento da Rocketseat. O nome é DoctorCare{" "}
+        </p>
+        <p>
+          <a href="">#novoprojeto</a>
+          <a href="">#nlw</a>
+          <a href="">#Rocketseat</a>
+        </p>
+      </div>
+    </article>
   );
 }
